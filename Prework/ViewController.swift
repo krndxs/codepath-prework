e45fe98643e887211ef623784b4a9d22290d176d
Karandeep Singh <53494842+krndxs@users.noreply.github.com>
2022-02-06 19:24:35 -0800

    Fixed display issues

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -2,36 +2,39 @@
 //  ViewController.swift
 //  Prework
 //
-//  Created by Karan on 2/6/22.
+//  Created by Karan
 //
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var timControl: UISegmentedControl!
-    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func calculateTip(_ sender: Any) {
-        //get Bil amount from text field input
+        // Get bill amount from text field input
         let bill = Double(billAmountTextField.text!) ?? 0
         
-        //Get total tip by multiplying tip * tipPercentage
+        // Get total tip by multiplying tip * tipPercentage
         let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill * tipPercentages[timControl.selectedSegmentIndex]
         let total = bill + tip
         
-        tipAmountLabel.text = String(format: "$%.2f")
-        totalLabel.text = String(format: "$%.2f")
+        // Update tip amount label
+        tipAmountLabel.text = String(format: "$%.2f",tip)
+        // Update total amount
+        totalLabel.text = String(format: "$%.2f", total)
         
     }
     
-}
 
+}
